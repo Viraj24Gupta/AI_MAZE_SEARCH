232cebde095432acbb39285b92e6439590eeaa34
Viraj24Gupta <61087588+Viraj24Gupta@users.noreply.github.com>
2020-10-15 20:40:04 +0530

    Add files via upload

--- new.js
+++ new.js
@@ -2,8 +2,3 @@ $(".dropContent0").click(function(){
     var x=$(this).text();
     $("#dropdownMenuButton0").text(x);
 });
-
-$(".dropContent").click(function(){
-    var x=$(this).text();
-    $("#dropdownMenuButton").text(x);
-});
