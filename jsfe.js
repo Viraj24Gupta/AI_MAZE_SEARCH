b252691e54d4bfbede8c95218a39f418877a0cc7
Viraj24Gupta <61087588+Viraj24Gupta@users.noreply.github.com>
2020-10-22 17:21:54 +0530

    Add files via upload

--- jsfe.js
+++ jsfe.js
@@ -3,18 +3,9 @@ $(".dropContent0").click(function() {
     $("#dropdownMenuButton0").text(x);
 });
 
-$(".dropContent0").click(function(){
-    const { exec } = require("child_process");
-
-    exec("g++ basic.cpp -o basic.exe", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
+$(document).ready(function(){
+    $("#menu-toggle").click(function(e){
+        e.preventDefault();
+        $("#wrapper").toggleClass("menuDisplayed");
     });
 });
